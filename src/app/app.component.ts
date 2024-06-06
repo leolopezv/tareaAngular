d5ffe1c0e22baa94e1760aeb15db16bf280c59d0
leolopezv <leonardo.lopez@uees.edu.ec>
2024-06-05 21:48:33 -0500

    Subida del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './services/user.service';
+import { User } from './models/user';
 import { RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule, HttpClientModule], // El import es para importar una librearia o modulo
+  providers: [UserService], // El provider es para importar un servicio
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
+
   title = 'tareaAngular';
+  users: User[] = [];
+
+  constructor(private userService: UserService) {
+    userService.obtenerDatos().subscribe(respuesta => {
+      this.users = respuesta as Array<User>;
+    })
+  }
+
+
 }
